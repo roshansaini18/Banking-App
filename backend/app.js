f97706cd9631ec2d8064a7e107c431e7081ff92d
roshansaini18 <135322753+roshansaini18@users.noreply.github.com>
2025-07-31 07:54:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -22,9 +22,17 @@ const authRouter = require('./routes/otp.routes');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors({ origin: 'http://localhost:5173', // no trailing slash
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",          // local dev
+      "https://sobank.netlify.app"      // deployed frontend
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
